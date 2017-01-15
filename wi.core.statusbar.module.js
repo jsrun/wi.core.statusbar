d668f85bb1c9c7f7fe6f18776b286619fd6f8857
Andre <Andre@DESKTOP-EVCUACT>
2017-01-15 03:32:03 -0200

    Compatibility changes

---
+++ wi.core.statusbar.module.js
@@ -0,0 +1,36 @@
+/**
+ *  __          __  _    _____ _____  ______    _____ _        _             _                
+ *  \ \        / / | |  |_   _|  __ \|  ____|  / ____| |      | |           | |               
+ *   \ \  /\  / /__| |__  | | | |  | | |__    | (___ | |_ __ _| |_ _   _ ___| |__   __ _ _ __ 
+ *    \ \/  \/ / _ \ '_ \ | | | |  | |  __|    \___ \| __/ _` | __| | | / __| '_ \ / _` | '__|
+ *     \  /\  /  __/ |_) || |_| |__| | |____ _ ____) | || (_| | |_| |_| \__ \ |_) | (_| | |   
+ *      \/  \/ \___|_.__/_____|_____/|______(_)_____/ \__\__,_|\__|\__,_|___/_.__/ \__,_|_|                                                                                                
+ *                                                                            
+ *  @author André Ferreira <andrehrf@gmail.com>
+ *  @license MIT
+ */
+
+"use strict";
+
+let TemplateEngine = require("../wi.core.template.js");
+
+module.exports = {    
+    /**
+     * List module assets
+     * @type object
+     */
+    assets: {
+        css: [__dirname + "/wi.core.statusbar.style.css"],
+        js: [__dirname + "/wi.core.statusbar.events.js"],
+    },
+    
+    /**
+     * Function to generate template
+     * 
+     * @param object _this
+     * @return string
+     */
+    getTemplate: function(_this){
+        return TemplateEngine(__dirname + "/wi.core.statusbar.tpl.ejs").seti18n(_this.i18n).render();
+    }
+};
